d50f753cc73502a2ea0467c5179cf09a48dd6b5b
whdudtjr <rtr894@naver.com>
2023-06-12 23:37:30 +0900

    #12 - 데스트 코드에 new line 추가
    깃헙 코드 리뷰에서 빨간 이모티콘 없애기 위함

--- fastcamus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ fastcamus-project-board/src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -95,6 +95,4 @@ class JpaRepositoryTest {
         assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
         assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentSize);
     }
-
-    
 }
