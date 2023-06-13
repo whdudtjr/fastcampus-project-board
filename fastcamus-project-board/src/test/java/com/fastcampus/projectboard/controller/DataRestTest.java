79a6944859a60bbf4a449f0804c18b8deaf20680
whdudtjr <rtr894@naver.com>
2023-06-14 08:05:41 +0900

    #17 - 회원 계정 도메인 구현 및 erd 반영
    `UserAccount` 이름으로 회원 계정 도메인 생성
    `user`, `account` 등은 mysql 예약어이므로 피함
    필드명도 mysql 예약어를 의식하여 정함
    
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

--- fastcamus-project-board/src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
+++ fastcamus-project-board/src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,11 +12,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
-import static io.micrometer.core.instrument.binder.http.HttpRequestTags.status;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @Disabled("Spring Data REST 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("Data REST - API 테스트")
@@ -38,8 +36,8 @@ public class DataRestTest {
         //Given
 
         //When & Then
-        mvc.perform(MockMvcRequestBuilders.get("/api/articles"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        mvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
@@ -49,8 +47,8 @@ public class DataRestTest {
         //Given
 
         //When & Then
-        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        mvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
@@ -60,8 +58,8 @@ public class DataRestTest {
         //Given
 
         //When & Then
-        mvc.perform(MockMvcRequestBuilders.get("/api/articles/1/articleComments"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        mvc.perform(get("/api/articles/1/articleComments"))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
@@ -71,8 +69,8 @@ public class DataRestTest {
         //Given
 
         //When & Then
-        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        mvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
 
@@ -82,9 +80,23 @@ public class DataRestTest {
         //Given
 
         //When & Then
-        mvc.perform(MockMvcRequestBuilders.get("/api/articleComments/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        mvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 회원 관련 API 는 일체 제공하지 않는다.")
+    @Test
+    void givenNothing_whenRequestingUserAccounts_thenThrowsException() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(post("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(put("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(patch("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(delete("/api/userAccounts")).andExpect(status().isNotFound());
+        mvc.perform(head("/api/userAccounts")).andExpect(status().isNotFound());
     }
 
 
