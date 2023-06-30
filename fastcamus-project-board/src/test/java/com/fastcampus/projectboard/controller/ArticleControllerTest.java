ea32c561da3e50fb68cb3219e0aaa0b8853c2e03
whdudtjr <rtr894@naver.com>
2023-06-30 21:51:32 +0900

    #26 - 게시글 페이지네이션 구현
    '이전 글' , '다음 글'만 구현하므로 게시판에 비해 단순하지만 첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리해야 해서 타임리프 문법이 복잡해짐 마지막 글을 판단하기 위해 총 글 개수가 필요해졌고 이를 위해 count 쿼리 사용, 서비스 로직 추가

--- fastcamus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ fastcamus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -97,22 +97,27 @@ class ArticleControllerTest {
     }
 
 
-    @DisplayName("[view][Get] 게시글 상세 페이지 - 정상 호출")
+    @DisplayName("[view][GET] 게시글 페이지 - 정상 호출")
     @Test
-    public void givenNothing_whenRequestArticleView_thenReturnsArticleView() throws Exception{
-        //Given
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
         Long articleId = 1L;
+        long totalCount = 1L;
         given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
-        //When & Then
+        given(articleService.getArticleCount()).willReturn(totalCount);
+
+        // When & Then
         mvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
-                .andExpect(model().attributeExists("articleComments"));
+                .andExpect(model().attributeExists("articleComments"))
+                .andExpect(model().attributeExists("articleComments"))
+                .andExpect(model().attribute("totalCount", totalCount));
         then(articleService).should().getArticle(articleId);
+        then(articleService).should().getArticleCount();
     }
-
     @Disabled("구현중")
     @DisplayName("[view][Get] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
